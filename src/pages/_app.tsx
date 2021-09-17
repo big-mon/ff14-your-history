ac66113f16e32784068017edc01c42bf1e5cbe23
BIG-MON <mrmon93+github@gmail.com>
2021-09-18 05:08:57 +0900

    build: add tailwind, typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
