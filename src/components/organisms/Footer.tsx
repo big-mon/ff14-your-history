54c089be253b8eae9e82de3f6e6a2bd007136f10
BIG-MON <mrmon93+github@gmail.com>
2021-09-19 16:38:45 +0900

    refactor: add option

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,16 +1,30 @@
 import { AuthorLink } from "../atoms/AuthorLink";
 import { CopyrightSQEX } from "../atoms/CopyrightSQEX";
 
-export const Footer = () => {
+type Prop = {
+  isSticky: boolean;
+};
+
+export const Footer = (props: Prop) => {
   return (
-    <div className="px-3 py-2 w-full fixed bottom-0 font-serif text-sm text-gray-400">
-      <div>
-        <span className="px-2 backdrop-blur">
-          Created by <AuthorLink />.
-        </span>
-        <br />
-        <CopyrightSQEX />
-      </div>
-    </div>
+    <>
+      {props.isSticky ? (
+        <div className="px-3 py-2 w-full fixed bottom-0 font-serif text-sm text-gray-400">
+          <span className="px-2 backdrop-blur">
+            Created by <AuthorLink />.
+          </span>
+          <br />
+          <CopyrightSQEX />
+        </div>
+      ) : (
+        <div className="px-3 py-2 w-full bottom-0 font-serif text-sm text-gray-400">
+          <span className="px-2 backdrop-blur">
+            Created by <AuthorLink />.
+          </span>
+          <br />
+          <CopyrightSQEX />
+        </div>
+      )}
+    </>
   );
 };
